3cde43f4dc1cfb594121c098a346bc9512c4d8c4
Petr Ryšavý <petr.rysavy@gmail.com>
2024-11-11 09:24:21 +0100

    IDE warnings

--- dagsolvers/magseparation.py
+++ dagsolvers/magseparation.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 
-def floydWarshall(adj):
+def floyd_warshall(adj):
     n = len(adj)
     dist = np.full((n, n), np.inf)
 
     # initialize the edges from the adj. matrix - inf where adj is zero, 1 otherwise
     dist[adj > 0.5] = 1
 
-    for k in range(n): # k is the midpoint of SP
+    for k in range(n):  # k is the midpoint of SP
         for i in range(n):
             for j in range(n):
-                len = dist[i][k] + dist[k][j]
-                if dist[i][j] > len:
-                    dist[i][j] = len
+                pathlen = dist[i][k] + dist[k][j]
+                if dist[i][j] > pathlen:
+                    dist[i][j] = pathlen
 
     return dist
 
 
-def traceFW(dist, u, v):
+def trace_f_w(dist, u, v):
     if dist[u][v] == np.inf:
         return []
 
@@ -26,23 +26,24 @@ def traceFW(dist, u, v):
     marked = np.full((n, n), False)
     edges = []
     stack = []
-    marked[u,v] = True
-    stack.append((u,v))
+    marked[u, v] = True
+    stack.append((u, v))
 
     while stack:
         i, j = stack.pop()
         for k in range(n):
-            if dist[i][k] + dist[k][j] == dist[i][j]: # an SP goes through k
+            if dist[i][k] + dist[k][j] == dist[i][j]:  # an SP goes through k
                 for s, t in [(i, k), (k, j)]:
-                    if not marked[s][t]: # make sure not to include duplicates
+                    if not marked[s][t]:  # make sure not to include duplicates
                         marked[s][t] = True
-                        if dist[s][t] == 1: # direct edge, add it to the list
-                            edges.add((s,t))
-                        else: # a path, recurse
+                        if dist[s][t] == 1:  # direct edge, add it to the list
+                            edges.append((s, t))
+                        else:  # a path, recurse
                             stack.append((s, t))
     return edges
 
-def inducing_path_dfs(dist, adj, s, u, possible_endpoints, path, only_one_dir = True):
+
+def inducing_path_dfs(dist, adj, s, u, possible_endpoints, path, only_one_dir=True):
     # dist = FW distances, adj = bidirect adjacency, u curr point, s where we started, path = current path
     if len(possible_endpoints) == 0:  # no possible enpoint, how can an inducing path exist?
         return []
@@ -75,8 +76,7 @@ def inducing_paths(dist, adjbi):
 
 
 def check_for_inducing_path(adj, adjbi):
-    n = len(adj)
-    fwdist = floydWarshall(adj)
+    fwdist = floyd_warshall(adj)
     paths = inducing_paths(fwdist, adjbi)
     retval = []  # list of tuples of lists (directed, bidirected)
     for path in paths:
@@ -85,10 +85,8 @@ def check_for_inducing_path(adj, adjbi):
         biedges = [(path[i], path[i + 1]) for i in range(len(path) - 1)]
         diredges = []
         for v in path[1:-1]:
-            diredges.extend(traceFW(fwdist, s, v))
-            diredges.extend(traceFW(fwdist, t, v))
+            diredges.extend(trace_f_w(fwdist, s, v))
+            diredges.extend(trace_f_w(fwdist, t, v))
         retval.append((diredges, biedges))
     return retval
     # todo implmement lazy constraints
-
-
